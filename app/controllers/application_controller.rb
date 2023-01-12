f8c61687ee68efa8bb2b2319491116025846d3ba
melashu <meshu102@outlook.com>
2023-01-12 09:13:40 +0300

    Add helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
+
 class ApplicationController < ActionController::Base
+
+  protect_from_forgery prepend: true
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :param_checker
+
+  def after_sign_in_path_for(_resource)
+    users_path
+  end
+
+  def must_log!
+    redirect_to user_session_path if current_user.nil?
+  end
+
+  protected
+
+  #  def param_checker
+  #       params.require(:user).permit(:icon, :name, :user_id)
+  #   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name photo])
+  end
 end
