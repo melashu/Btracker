2d975f6da809f022bd67175be31420c82abd1eb8
melashu <meshu102@outlook.com>
2023-01-12 09:14:18 +0300

    Add transactions model

---
+++ app/models/btransaction.rb
@@ -0,0 +1,7 @@
+class Btransaction < ApplicationRecord
+  belongs_to :author, class_name: :User, foreign_key: :author_id
+  has_many :catagory_transactions
+  has_many :catagories, through: :catagory_transactions
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+end
