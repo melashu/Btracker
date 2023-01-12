161b39bf4c3bb46b7fdd7e85c144ce70612b33b9
melashu <meshu102@outlook.com>
2023-01-12 11:45:54 +0300

    Add mailer for production

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'user.info@btracker.com'
+  config.mailer_sender = 'btracker@btracker.onrender.com/'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
