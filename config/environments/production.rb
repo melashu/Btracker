7440eaf6c0ddab3ef15fd6760b6a459edbd2503d
melashu <meshu102@outlook.com>
2023-01-16 12:20:24 +0300

    Add mailer configration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,5 +90,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'https://btracker.onrender.com', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'btracker.onrender.com', port: 3000 }
+  config.action_controller.default_url_options = { host: 'btracker.onrender.com', port: 3000 }
+
+
 end
