f39a22f0d2beaa2e440153dc631e4fc6933a4dc1
melashu <meshu102@outlook.com>
2023-02-14 09:25:57 +0300

    Add linter fix

--- app/models/btransaction.rb
+++ app/models/btransaction.rb
@@ -4,7 +4,4 @@ class Btransaction < ApplicationRecord
   has_many :catagories, through: :catagory_transactions
   validates :name, presence: true, length: { maximum: 255 }
   validates :amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  # def as_json(option={})
-  #   {:id=>self.id, :name=>self.name, :amount=>self.amount}
-  # end
 end
