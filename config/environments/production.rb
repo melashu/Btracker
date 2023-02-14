6322f8af016e932d7773714f8e01acb37cdfaf82
melashu <meshu102@outlook.com>
2023-02-14 15:55:02 +0300

    Add configration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -90,8 +90,19 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'btracker.onrender.com', port: 3000 }
-  config.action_controller.default_url_options = { host: 'btracker.onrender.com', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'btracker.onrender.com' }
+  config.action_controller.default_url_options = { host: 'btracker.onrender.com'}
 
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'btracker.onrender.com',
+  user_name:            'meshu102@gmail.com'
+  password:             'oczkkckgtbhgmzmd', #vrioxhhrgvrguazi
+  authentication:       'plain',
+  enable_starttls_auto: true,
+  open_timeout:         5,
+  read_timeout:         5 }
 
 end
