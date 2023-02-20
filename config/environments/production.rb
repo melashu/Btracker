cbda425d5a6bc36708f9b05d8e73571feeb9b9d0
melashu <meshu102@outlook.com>
2023-02-20 09:01:30 +0300

    Add email configration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "Btracker_production"
 
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.perform_deliveries = true
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
